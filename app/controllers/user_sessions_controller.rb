bf4232bfba1dbaacd57a2fd0c5a252b12469838e
ekoki <kt.alive.0922@gmail.com>
2023-03-27 21:37:50 +0900

    Add: ログイン機能の実装 #11

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,15 @@
+class UserSessionsController < ApplicationController
+
+  def new;end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to root_path, notice: t('.success')
+    else
+      flash.now[:alert] = t('.fail')
+      render :new
+    end
+  end
+
+end

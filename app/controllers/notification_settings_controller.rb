2f47c5fdc7d107dd787c93fda5417b445abc7b2d
ekoki <kt.alive.0922@gmail.com>
2023-06-16 23:27:37 +0900

    Fix: Jobコードの修正

--- app/controllers/notification_settings_controller.rb
+++ app/controllers/notification_settings_controller.rb
@@ -8,8 +8,7 @@ class NotificationSettingsController < ApplicationController
     if @notification_setting.save
       send_time = @notification_setting.send_time
       # 下記コードにより、send_time時間になるとSendLineMessageJobが実行される。
-      # SendLineMessageJob.set(wait_until: send_time).perform_later(@notification_setting.id)
-      SendLineMessageJob.perform_now(@notification_setting.id)
+      SendLineMessageJob.set(wait_until: send_time).perform_later(@notification_setting.id)
       redirect_to new_question_path, notice: t('.success')
     else
       flash.now[:fail] = t('.fail')

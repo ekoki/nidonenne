f70dae7155c0fcec8dd875f63a4cebed3e863689
ekoki <kt.alive.0922@gmail.com>
2023-07-12 10:56:02 +0900

    Fix:seedファイル

--- db/seed.rb
+++ db/seed.rb
@@ -1,5 +1,5 @@
 require "csv"
 
 CSV.foreach('db/English_word.csv') do |row|
-AutomaticQuestion.create!(id => row[2], :question => row[0], :answer => row[1])
+AutomaticQuestion.create!(:id => row[2], :question => row[0], :answer => row[1])
 end

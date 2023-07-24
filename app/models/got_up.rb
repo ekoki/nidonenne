4a73108c7817fdb48fa94cbe69a09d2c9ee59b08
ekoki <kt.alive.0922@gmail.com>
2023-07-25 02:03:31 +0900

    Fix: スケジュールの修正

--- app/models/got_up.rb
+++ app/models/got_up.rb
@@ -8,10 +8,12 @@ class GotUp < ApplicationRecord
     if user.notification_settings.first.send_time.nil?
       return false
     else
-      send_time_plus = user.notification_settings.first.send_time + 10.minutes
-      send_time = send_time_plus.strftime("%H:%M:%S")
+      sending_time = user.notification_settings.first.send_time
+      sending_time_now = sending_time.strftime("%H:%M:%S")
+      send_time = user.notification_settings.first.send_time + 10.minutes
+      send_time_plus = send_time.strftime("%H:%M:%S")
     end
-    if  current_time < send_time
+    if  sending_time_now <= current_time &&  current_time < send_time_plus
       GotUp.create!(user_id: user.id, got_up: true, start_time: Time.current)
     end
   end

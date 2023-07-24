28125601b02d6fbd201849cae5d3d43a79c4a302
ekoki <kt.alive.0922@gmail.com>
2023-07-24 23:27:40 +0900

    Fix: seedファイルの修正

--- db/seed.rb
+++ db/seed.rb
@@ -1,5 +1,5 @@
 require "csv"
 
 CSV.foreach(Rails.root.join('db', 'English_word.csv')) do |row|
-AutomaticQuestion.create!(:id => row[2], :question => row[0], :answer => row[1])
+AutomaticQuestion.create!(:id => row[2], :question => row[1], :answer => row[0])
 end

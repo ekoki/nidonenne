40dfeaead59856e0e1db479e3dd286eda2312f5c
ekoki <kt.alive.0922@gmail.com>
2023-07-25 18:27:19 +0900

    Add: metaタグの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,46 @@ module ApplicationHelper
     else 'alert'
     end
   end
+
+
+  def default_meta_tags
+    {
+      site: 'にどねんね',
+      title: '二度寝を防止しよう！',
+      reverse: true,
+      separator: '|',   #Webサイト名とページタイトルを区切るために使用されるテキスト
+      description: '二度寝を防止するアプリです',
+      keywords: 'にどねんね',   #キーワードを「,」区切りで設定する
+      canonical: 'https://www.nidonenne.com',
+      noindex: ! Rails.env.production?,
+      icon: [                    #favicon、apple用アイコンを指定する
+        { href: image_url('jordan-wozniak-xP_AGmeEa6s-unsplash.jpg') },
+      ],
+      og: {
+        site_name: 'にどねんね',
+        title: '二度寝を防止しよう！',
+        description: '二度寝を防止するアプリです', 
+        type: 'website',
+        url: 'https://www.nidonenne.com',
+        image: image_url('jordan-wozniak-xP_AGmeEa6s-unsplash.jpg'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@ツイッターのアカウント名',
+        image: image_url('jordan-wozniak-xP_AGmeEa6s-unsplash.jpg')
+      }
+    }
+  end
+
+
+
+
+
+
+
+
+
+
+
 end

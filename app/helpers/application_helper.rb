d5611e24870a74a7ee070d83a7718bf9a27e1cdb
ekoki <kt.alive.0922@gmail.com>
2023-07-26 01:04:42 +0900

    Add: metaタグの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,38 +13,29 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'にどねんね',
-      title: 'にどねんね',
+      title: '二度寝防止のためのサービス',
       reverse: true,
       separator: '|',   #Webサイト名とページタイトルを区切るために使用されるテキスト
-      description: 'にどねんねは二度寝を防止の防止を手助けするアプリです！',
+      description: 'にどねんねは二度寝の防止を手助けするアプリです！ラインと連携することにより指定した時間帯に問題が届きます。このアプリを利用して朝活をより良いものにしていきましょう。',
       keywords: 'にどねんね, 二度寝',   #キーワードを「,」区切りで設定する
-      canonical: 'https://www.nidonenne.com',
+      canonical: request.original_url,
       noindex: ! Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+      ],
       og: {
         site_name: 'にどねんね',
-        title: '二度寝を防止しよう！',
-        description: '二度寝を防止するアプリです', 
+        title: :title,
+        description: 'にどねんねは二度寝の防止を手助けするアプリです！ラインと連携することにより指定した時間帯に問題が届きます。このアプリを利用して朝活をより良いものにしていきましょう。', 
         type: 'website',
-        url: 'https://www.nidonenne.com',
+        url: request.original_url,
         image: image_url('opg.png'),
         locale: 'ja_JP',
       },
       twitter: {
         card: 'summary_large_image',
-        site: '@ツイッターのアカウント名',
         image: image_url('opg.png')
       }
     }
   end
-
-
-
-
-
-
-
-
-
-
-
 end

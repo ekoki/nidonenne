2cd4c8aaf88e418c9f2c781dade5a029884acaa6
ekoki <kt.alive.0922@gmail.com>
2023-08-17 02:12:06 +0900

    Fix: 通知画面の修正

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,7 @@
 class SchedulesController < ApplicationController
   def index
     @got_ups = GotUp.where(user_id: current_user.id)
+    @notification = current_user.notification_settings.first
   end
 
  

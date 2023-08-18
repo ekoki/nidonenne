30b8f216b4998286a24344c3a6289f861f0e9898
ekoki <kt.alive.0922@gmail.com>
2023-08-18 23:53:21 +0900

    Fix: 通知設定

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,7 +1,6 @@
 class SchedulesController < ApplicationController
   def index
     @got_ups = GotUp.where(user_id: current_user.id)
-    @notification = current_user.notification_settings.first
   end
 
  
